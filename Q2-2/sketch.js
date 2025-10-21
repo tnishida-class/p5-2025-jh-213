c5dc2eda303ff88a832367f1d964a8908fe7dd61
jh-213 <252ce22c@gsuite.kobe-u.ac.jp>
2025-10-21 11:33:19 +0900

    Change section1-1

--- Q2-2/sketch.js
+++ Q2-2/sketch.js
@@ -1,11 +1,12 @@
 // チェッカー
 function setup() {
   createCanvas(200, 200);
-  const size = width / 8; // マスの一辺の長さ
-  noStroke();
-  for(let i = 0; i < 8; i++){
-    for(let j = 0; j < 8; j++){
-      // BLANK[1] ヒント： rectのx座標は size * i, y座標は size * j
-    }
-  }
-}
+  fill(0);
+      const size = width / 8; // マスの一辺の長さ
+    //noStroke();
+    for(let i = 0; i < 8; i++){
+      for(let j = 0; j < 8; j++){
+        // BLANK[1] ヒント： rectのx座標は size * i, y座標は size * j
+       fill(255, 0, 0);
+     rect(size * i, size * j, size, size);
+    } 
