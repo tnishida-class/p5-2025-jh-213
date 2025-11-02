3c7e46bf6328fcde4ead1227b43e71d825a5e34a
jh-213 <252ce22c@gsuite.kobe-u.ac.jp>
2025-11-03 00:37:15 +0900

    Update sketch.js

--- Q3-1/sketch.js
+++ Q3-1/sketch.js
@@ -1,6 +1,7 @@
 // 心臓の鼓動のようなアニメーション
 const cycle = 100; // 1周期のフレーム数
 let count; // 何フレーム目か
+let speed = 1; // アニメーションの速さ
 
 function setup(){
   createCanvas(200, 200);
@@ -9,12 +10,21 @@ function setup(){
 
 function draw(){
   background(160, 192, 255);
-  
-  let speed = 1; // アニメーションの速さ
-  // BLANK[2]
-  count = (count + speed) % cycle;
-
   let size = 50;
-  // BLANK[1] 1周期の前半は size が大きくなり、後半は小さくなる
-  ellipse(width / 2, height / 2, size);
-}
+  if (count < cycle / 2){
+    size += 5;
+  } else {
+    size -= 5;
+  }// BLANK[1] 1周期の前半は size が大きくなり、後半は小さくなる
+count = (count + speed) % cycle;
+  ellipse(width / 2, height / 2, size); 
+  }
+
+  function keyPressed(){
+    speed = 5;} // BLANK[2]
+  
+  function keyReleased(){
+    speed = 1};
+
+
+  
